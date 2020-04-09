bf25953bd70e4a62119bfed2f395e70b08323710
Dima <dima23790@gmail.com>
2020-04-09 21:44:04 +0300

    Updates

---
+++ precache-manifest.da0873b13a6d2901cb0fae45aab1192b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d317e2a5ef1abeeddb145098004bd5",
+    "url": "/burgerBuilder/index.html"
+  },
+  {
+    "revision": "9c697eac04623f54345c",
+    "url": "/burgerBuilder/static/css/main.b801dfcf.chunk.css"
+  },
+  {
+    "revision": "c2eeedf41654468a6969",
+    "url": "/burgerBuilder/static/js/2.d0894fba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burgerBuilder/static/js/2.d0894fba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c697eac04623f54345c",
+    "url": "/burgerBuilder/static/js/main.a4250995.chunk.js"
+  },
+  {
+    "revision": "5d013b47a65a993b6b26",
+    "url": "/burgerBuilder/static/js/runtime-main.a5c86951.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
